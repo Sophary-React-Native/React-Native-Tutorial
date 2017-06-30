922c55d9947d0cc3b1f47b153a6b71705a973f2c
SopharyMSH <sopharydev@gmail.com>
2017-06-30 10:12:12 +0700

    section list

--- index.ios.js
+++ index.ios.js
@@ -4,7 +4,7 @@ import {
   ScrollView,
   Image,
   Text,
-  FlatList,
+  SectionList,
   StyleSheet,
   View
 } from 'react-native';
@@ -15,21 +15,16 @@ export default class heighAndWidth extends Component {
   }
    render() {
      return (
-      <View style={styles.container}>
-        <FlatList
-          data={[
-            {key: 'Devin'},
-            {key: 'Jackson'},
-            {key: 'James'},
-            {key: 'Joel'},
-            {key: 'John'},
-            {key: 'Jillian'},
-            {key: 'Jimmy'},
-            {key: 'Julie'},
-          ]}
-          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
-        />
-      </View>
+       <View style={styles.container}>
+         <SectionList
+           sections={[
+             {title: 'D', data: ['Devin']},
+             {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+           ]}
+           renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+           renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+         />
+       </View>
     );
   }
 }
@@ -38,12 +33,20 @@ const styles = StyleSheet.create({
    flex: 1,
    paddingTop: 22
   },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
   item: {
     padding: 10,
     fontSize: 18,
     height: 44,
   },
 })
-
 
 AppRegistry.registerComponent('heighAndWidth', () => heighAndWidth);
