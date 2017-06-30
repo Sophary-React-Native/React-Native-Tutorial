6b82db4592d3c32502bb130c79f5c655d827adae
SopharyMSH <sopharydev@gmail.com>
2017-06-30 09:39:58 +0700

    flexdirection

--- index.ios.js
+++ index.ios.js
@@ -7,10 +7,10 @@ import {
 export default class heighAndWidth extends Component {
   render() {
     return (
-      <View style={{flex: 1}}>
-        <View style={{flex: 1 , backgroundColor: 'powderblue'}} />
-        <View style={{flex: 1 , backgroundColor: 'skyblue'}} />
-        <View style={{flex: 1 , backgroundColor: 'steelblue'}} />
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
