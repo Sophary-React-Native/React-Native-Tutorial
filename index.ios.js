97b97a0d122ab16cfcd55d3bd887d1d43dacf2aa
SopharyMSH <sopharydev@gmail.com>
2017-06-30 09:42:03 +0700

    display in column and has space between

--- index.ios.js
+++ index.ios.js
@@ -7,7 +7,11 @@ import {
 export default class heighAndWidth extends Component {
   render() {
     return (
-      <View style={{flex: 1, flexDirection: 'row'}}>
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}>
         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
         <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
         <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
