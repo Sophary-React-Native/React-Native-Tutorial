e1c0c45471baea58e53660ced57de8ab2a91305a
SopharyMSH <sopharydev@gmail.com>
2017-06-30 10:00:31 +0700

    handling touches

--- index.ios.js
+++ index.ios.js
@@ -1,19 +1,61 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  View
+  View,
+  Button,
+  StyleSheet,
+  Alert
 } from 'react-native';
 
 export default class heighAndWidth extends Component {
-  render() {
-    return (
-      <View style={{flex: 1}}>
-        <View style={{flex: 1 , backgroundColor: 'powderblue'}} />
-        <View style={{flex: 1 , backgroundColor: 'skyblue'}} />
-        <View style={{flex: 1 , backgroundColor: 'steelblue'}} />
-      </View>
-    );
+  _onPressButton() {
+    Alert.alert('You tapped the button');
+  }
+   render() {
+     return (
+       <View style={styles.container}>
+         <View style={styles.buttonContainer}>
+           <Button
+             onPress={this._onPressButton}
+             title="Press Me"
+           />
+         </View>
+         <View style={styles.buttonContainer}>
+           <Button
+             onPress={this._onPressButton}
+             title="Press Me"
+             color="#841584"
+           />
+         </View>
+         <View style={styles.alternativeLayoutButtonContainer}>
+           <Button
+             onPress={this._onPressButton}
+             title="This looks great!"
+           />
+           <Button
+             onPress={this._onPressButton}
+             title="OK!"
+             color="#841584"
+           />
+         </View>
+       </View>
+     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 20
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+})
+
 
 AppRegistry.registerComponent('heighAndWidth', () => heighAndWidth);
