02fe6679ce1ed6f8843e6944f2953d3914df83f5
SopharyMSH <sopharydev@gmail.com>
2017-06-30 10:10:38 +0700

    flat list

--- index.ios.js
+++ index.ios.js
@@ -3,7 +3,10 @@ import {
   AppRegistry,
   ScrollView,
   Image,
-  Text
+  Text,
+  FlatList,
+  StyleSheet,
+  View
 } from 'react-native';
 
 export default class heighAndWidth extends Component {
@@ -12,42 +15,35 @@ export default class heighAndWidth extends Component {
   }
    render() {
      return (
-        <ScrollView>
-          <Text style={{fontSize:96}}>Scroll me plz</Text>
-          <Image source={require('./img/images.png')} />
-          <Image source={require('./img/images.png')} />
-          <Image source={require('./img/images.png')} />
-          <Image source={require('./img/images.png')} />
-          <Image source={require('./img/images.png')} />
-          <Text style={{fontSize:96}}>If you like</Text>
-          <Image source={require('./img/images.png')} />
-          <Image source={require('./img/images.png')} />
-          <Image source={require('./img/images.png')} />
-          <Image source={require('./img/images.png')} />
-          <Image source={require('./img/images.png')} />
-          <Text style={{fontSize:96}}>Scrolling down</Text>
-          <Image source={require('./img/images.png')} />
-          <Image source={require('./img/images.png')} />
-          <Image source={require('./img/images.png')} />
-          <Image source={require('./img/images.png')} />
-          <Image source={require('./img/images.png')} />
-          <Text style={{fontSize:96}}>What is the best</Text>
-          <Image source={require('./img/images.png')} />
-          <Image source={require('./img/images.png')} />
-          <Image source={require('./img/images.png')} />
-          <Image source={require('./img/images.png')} />
-          <Image source={require('./img/images.png')} />
-          <Text style={{fontSize:96}}>Framework around?</Text>
-          <Image source={require('./img/images.png')} />
-          <Image source={require('./img/images.png')} />
-          <Image source={require('./img/images.png')} />
-          <Image source={require('./img/images.png')} />
-          <Image source={require('./img/images.png')} />
-          <Text style={{fontSize:80}}>React Native</Text>
-        </ScrollView>
+      <View style={styles.container}>
+        <FlatList
+          data={[
+            {key: 'Devin'},
+            {key: 'Jackson'},
+            {key: 'James'},
+            {key: 'Joel'},
+            {key: 'John'},
+            {key: 'Jillian'},
+            {key: 'Jimmy'},
+            {key: 'Julie'},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
+      </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
 
 
 AppRegistry.registerComponent('heighAndWidth', () => heighAndWidth);
